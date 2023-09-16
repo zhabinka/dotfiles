8f006f3149b0a85465bc0cabd0889c89e54faa13
Sergey Karpuk <sergeykarpuk@yandex.by>
2023-09-16 18:33:52 +0300

    feature(treesitter): add language modules to preload

--- nvim/lua/user/plugins/treesitter.lua
+++ nvim/lua/user/plugins/treesitter.lua
@@ -3,7 +3,18 @@ return {
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-      -- "lua"
+      "lua",
+      "elixir",
+      "erlang",
+      "bash",
+      "dockerfile",
+      "git_config",
+      "gitignore",
+      "gitattributes",
+      "make",
+      "markdown",
+      "sql",
+      "yaml",
     })
   end,
 }
