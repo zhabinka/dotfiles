a06bcc7a4586e041d972372b5e7f8f6c97d1e944
Sergey Karpuk <sergeykarpuk@yandex.by>
2023-09-16 18:09:04 +0300

    feature(mason): add elixir, erlang, lua plugin

--- nvim/lua/user/plugins/mason.lua
+++ nvim/lua/user/plugins/mason.lua
@@ -7,7 +7,9 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "lua_ls",
+        "elixirls",
+        "erg_language_server",
+        "lua_ls",
       })
     end,
   },
@@ -18,8 +20,7 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "prettier",
-        -- "stylua",
+        "nextls",
       })
     end,
   },
@@ -29,7 +30,8 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "python",
+        "elixirls",
+        "erg_language_server",
       })
     end,
   },
