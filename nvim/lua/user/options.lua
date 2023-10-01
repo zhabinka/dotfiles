36c8c4db643af25630ad180857ab4a4f4679aff8
Sergey Karpuk <sergeykarpuk@yandex.by>
2023-10-01 12:44:37 +0300

    feature(options): set dark theme

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -22,6 +22,9 @@ return {
     ui_notifications_enabled = true, -- disable notifications when toggling UI elements
     resession_enabled = false,       -- enable experimental resession.nvim session management (will be default in AstroNvim v4)
   },
+  -- o = {
+  --   background = "dark",
+  -- },
 }
 -- If you need more control, you can use the function()...end notation
 -- return function(local_vim)
