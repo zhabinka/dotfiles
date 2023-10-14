b083bbd540571790bd470b53492d37c36360c801
Sergey Karpuk <sergeykarpuk@yandex.by>
2023-10-14 21:44:50 +0300

    fix(treesitter): add heex and another highlight

--- nvim/lua/user/plugins/treesitter.lua
+++ nvim/lua/user/plugins/treesitter.lua
@@ -1,10 +1,13 @@
 return {
+  -- https://github.com/nvim-treesitter/nvim-treesitter
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
       "lua",
       "elixir",
+      "eex",
+      "heex",
       "erlang",
       "bash",
       "dockerfile",
@@ -15,6 +18,11 @@ return {
       "markdown",
       "sql",
       "yaml",
+      "json",
+      "html",
+      "python",
+      "toml",
+      "org", -- https://github.com/milisims/tree-sitter-org
     })
   end,
 }
