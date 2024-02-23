23c3ccf961d0737e41f87cbd650b43eb7de21c5d
Sergey Karpuk <sergeykarpuk@yandex.by>
2024-02-23 13:40:57 +0300

    feature(plugin): add obs (obsidian)

---
+++ nvim/lua/user/plugins/obs.lua
@@ -0,0 +1,37 @@
+-- https://github.com/IlyasYOY/obs.nvim
+return {
+    "IlyasYOY/obs.nvim",
+    event = "VeryLazy",
+    dependencies = {
+        "IlyasYOY/coredor.nvim",
+        -- "nvim-lua/plenary.nvim",
+        -- "nvim-telescope/telescope.nvim",
+    },
+    -- dev = true,
+    config = function()
+        local obs = require "obs"
+
+        obs.setup {
+            vault_home = "~/notes",
+            vault_name = "notes",
+            journal = {
+                template_name = "daily",
+            },
+        }
+
+        vim.keymap.set("n", "<leader>nn", "<cmd>ObsNvimFollowLink<cr>")
+        vim.keymap.set("n", "<leader>nr", "<cmd>ObsNvimRandomNote<cr>")
+        vim.keymap.set("n", "<leader>nN", "<cmd>ObsNvimNewNote<cr>")
+        vim.keymap.set("n", "<leader>ny", "<cmd>ObsNvimCopyObsidianLinkToNote<cr>")
+        vim.keymap.set("n", "<leader>no", "<cmd>ObsNvimOpenInObsidian<cr>")
+        vim.keymap.set("n", "<leader>nd", "<cmd>ObsNvimDailyNote<cr>")
+        vim.keymap.set("n", "<leader>nw", "<cmd>ObsNvimWeeklyNote<cr>")
+        vim.keymap.set("n", "<leader>nrn", "<cmd>ObsNvimRename<cr>")
+        vim.keymap.set("n", "<leader>nT", "<cmd>ObsNvimTemplate<cr>")
+        vim.keymap.set("n", "<leader>nM", "<cmd>ObsNvimMove<cr>")
+        vim.keymap.set("n", "<leader>nb", "<cmd>ObsNvimBacklinks<cr>")
+        vim.keymap.set("n", "<leader>nfj", "<cmd>ObsNvimFindInJournal<cr>")
+        vim.keymap.set("n", "<leader>nff", "<cmd>ObsNvimFindNote<cr>")
+        vim.keymap.set("n", "<leader>nfg", "<cmd>ObsNvimFindInNotes<cr>")
+    end,
+}
