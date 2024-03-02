ae26be3fbfcac4554f9fdcb7ddf8ff465d66c668
Sergey Karpuk <sergeykarpuk@yandex.by>
2024-03-02 22:00:33 +0300

    feature(plagins): add vim-dadbod-ui

---
+++ nvim/lua/user/plugins/vim-dadbod-ui.lua
@@ -0,0 +1,22 @@
+return {
+  -- https://github.com/kristijanhusak/vim-dadbod-ui
+  'kristijanhusak/vim-dadbod-ui',
+  event = 'VeryLazy',
+  ft = { 'sql', 'mysql', 'plsql' },
+  dependencies = {
+    { 'tpope/vim-dadbod' },
+    -- https://github.com/kristijanhusak/vim-dadbod-completion
+    { 'kristijanhusak/vim-dadbod-completion' },
+  },
+  cmd = {
+    'DBUI',
+    'DBUIToggle',
+    'DBUIAddConnection',
+    'DBUIFindBuffer',
+  },
+  init = function()
+    -- Your DBUI configuration
+    vim.g.db_ui_use_nerd_fonts = 1
+    vim.g.db_ui_save_location = "~/sql/"
+  end,
+}
