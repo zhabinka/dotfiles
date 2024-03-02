93b9bfd73da8f65c69fbdf50ea26909eb9c1147f
Sergey Karpuk <sergeykarpuk@yandex.by>
2024-03-02 23:25:49 +0300

    feature(cmp): config vim-dadbod-completion

--- nvim/lua/user/plugins/vim-dadbod-ui.lua
+++ nvim/lua/user/plugins/vim-dadbod-ui.lua
@@ -2,7 +2,6 @@ return {
   -- https://github.com/kristijanhusak/vim-dadbod-ui
   'kristijanhusak/vim-dadbod-ui',
   event = 'VeryLazy',
-  ft = { 'sql', 'mysql', 'plsql' },
   dependencies = {
     { 'tpope/vim-dadbod' },
     -- https://github.com/kristijanhusak/vim-dadbod-completion
