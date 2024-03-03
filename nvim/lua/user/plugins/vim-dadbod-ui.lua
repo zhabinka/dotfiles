6baeca807a50e4c0b4b135810e5c61b136dd6c10
Sergey Karpuk <sergeykarpuk@yandex.by>
2024-03-03 21:50:23 +0300

    fix(nvim-notify): move block to right bottom corner

--- nvim/lua/user/plugins/vim-dadbod-ui.lua
+++ nvim/lua/user/plugins/vim-dadbod-ui.lua
@@ -17,7 +17,7 @@ return {
     vim.g.db_ui_win_position = 'right'
     vim.g.db_ui_show_help = 0
     vim.g.db_ui_use_nerd_fonts = 1
-    vim.g.db_ui_use_nvim_notify = 0
+    vim.g.db_ui_use_nvim_notify = 1
     vim.g.db_ui_save_location = "~/sql/"
     vim.g.db_ui_tmp_query_location = '~/sql/tmp/'
   end,
