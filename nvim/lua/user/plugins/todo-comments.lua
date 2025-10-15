a1f946124e2f641acbcbc652ac85ac86660be891
Sergey Karpuk <sergeykarpuk@yandex.by>
2025-10-15 17:39:52 +0300

    refactor(todo-comments): add docs marker

--- nvim/lua/user/plugins/todo-comments.lua
+++ nvim/lua/user/plugins/todo-comments.lua
@@ -1,18 +1,19 @@
 return
 {
-  -- https://github.com/folke/todo-comments.nvim
+  -- DOC: https://github.com/folke/todo-comments.nvim
   "folke/todo-comments.nvim",
   event = "User AstroFile",
   dependencies = { "nvim-lua/plenary.nvim" },
   opts = {
     keywords = {
-      TODO = { icon = " " },
+      TODO = { icon = " ", color = "#fb4934" },
       HACK = { icon = " "},
-      WARN = { icon = " ", color = "#cc241d" },
+      WARN = { icon = " ", color = "#9d0006" },
       PERF = { icon = " ", color = "#8f3f71" },
       TEST = { icon = " ", color = "#d65d0e" },
       NOTE = { icon = " ", alt = { "INFO" } },
-      TEMP = { icon = " ", color = "#7c6f64" },
+      TEMP = { icon = " ", color = "#282828" },
+      DOCS = { icon = " ", color = "#a89984"}
     },
   },
 }
